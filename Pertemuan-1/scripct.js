e08df298f937b22ff37e9586b60dc79b33b8c3b8
taofikapriana <muhammadtaofikaprianasurya@gmail.com>
2024-10-10 11:39:01 +0700

    commit 9 menampil angka acak

--- Pertemuan-1/scripct.js
+++ Pertemuan-1/scripct.js
@@ -6,3 +6,8 @@ document.getElementById('btnClick').addEventListener('click', function() {
     const now = new Date();
     alert('Tombol telah diklik! Waktu saat ini: ' + now.toLocaleTimeString());
 });
+
+document.getElementById('btnClick').addEventListener('click', function() {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    alert('Angka acak: ' + randomNumber);
+});
