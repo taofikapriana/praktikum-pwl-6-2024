dcb356cc2656d7cb9d29d847f14edc4f0b8acb69
taofikapriana <muhammadtaofikaprianasurya@gmail.com>
2024-10-10 11:38:07 +0700

    commit 8 Menampilkan waktu saat tombol diklik

--- Pertemuan-1/scripct.js
+++ Pertemuan-1/scripct.js
@@ -1,3 +1,8 @@
 document.getElementById('btnClick').addEventListener('click', function() {
     alert('Praktikum PWL-6');
 });
+
+document.getElementById('btnClick').addEventListener('click', function() {
+    const now = new Date();
+    alert('Tombol telah diklik! Waktu saat ini: ' + now.toLocaleTimeString());
+});
